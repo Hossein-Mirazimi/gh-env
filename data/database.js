96be9fafa17d8aa2169fd3f93b6f91a07f487938
Hossein Mirazimi <h.mirazimi@alibaba.ir>
2023-04-29 10:56:41 +0330

    update workflow

--- data/database.js
+++ data/database.js
@@ -1,11 +1,12 @@
 import { MongoClient } from 'mongodb';
 
+const clusterProtocol = process.env.MONGODB_CLUSTER_PROTOCOL || 'mongodb+srv';
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `${clusterProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { connectTimeoutMS: 3000 });
 
 console.log('Trying to connect to db');
